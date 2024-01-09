cab4101733270d3d8d704a3df6022bf0d0f788a9
dbstocker <coffeemetalcode@gmail.com>
2024-01-09 05:53:19 -0500

    basic setup

---
+++ scratch/src/main.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Module } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+@Controller()
+class AppController {
+
+  @Get()
+  getRootRoute(): string {
+    return 'Hello there!';
+  }
+}
+
+@Module({
+  controllers: [ AppController ]
+})
+class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(3006);
+}
+
+bootstrap();
