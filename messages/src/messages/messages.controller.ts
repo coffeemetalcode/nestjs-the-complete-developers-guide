6480929176e0136217cbbfd01a6c9059f14fa409
dbstocker <coffeemetalcode@gmail.com>
2024-01-10 20:07:30 -0500

    finish section 3

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
-  @Get('/messages')
-  getMessages(): string {
-    return 'Messages route works!';
+  @Get()
+  getMessages(): void {
+    console.log('Getting all messages!');
+  }
+
+  @Get('/:id')
+  getMessage(): void {
+    console.log('Getting message');
+  }
+
+  @Post()
+  createMessage(): void {
+    console.log('Message created');
   }
 }
