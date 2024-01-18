86d7da5fb6ec663d714dbed239617cb01412f45d
dbstocker <coffeemetalcode@gmail.com>
2024-01-17 21:56:06 -0500

    mysql setup with TypeOrm

--- autovaluator/src/app.module.ts
+++ autovaluator/src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 import { ReportsModule } from './reports/reports.module';
+import { Report } from './reports/report.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      database: 'auto_valuator_instance',
+      entities: [Report, User],
+      synchronize: true,
+      host: '127.0.0.1', // <-- changed from 'localhost'
+      port: 3306,
+      username: 'avapp',
+      password: 'Test123$',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
